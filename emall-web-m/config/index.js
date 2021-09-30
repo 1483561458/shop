4c8c1cb15706f0c3b054aa374563c9a314baceb3
1483561458 <1483561458@qq.com>
2021-09-30 20:46:00 +0800

    初始化项目

---
+++ emall-web-m/config/index.js
@@ -0,0 +1,11 @@
+
+// export const VUE_APP_API_URL = 'http://natapp.xinxintuan.co/api';
+//export const VUE_APP_API_URL = 'https://wxapi.yixiang.co/api';
+export const VUE_APP_API_URL = 'http://192.168.8.50:8008/api';
+// export const VUE_APP_API_URL = 'http://192.168.31.223:8008/api';
+// export const VUE_APP_API_URL = 'https://h5api.xinxintuan.co/api';
+// export const VUE_APP_API_URL = 'https://h5api.xinxintuan.co/api';
+export const VUE_APP_RESOURCES_URL = 'https://wx.yixiang.co/static';
+
+// test API
+// export const VUE_APP_API_URL = 'http://36.133.8.121:8008/api';
